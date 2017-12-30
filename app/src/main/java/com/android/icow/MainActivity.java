68c22a33d2f9a0072c6d7de2ee620458afb10223
maxtaube <max.taube@hotmail.com>
2017-12-30 23:03:21 +0100

    change max taube 2017

--- app/src/main/java/com/android/icow/MainActivity.java
+++ app/src/main/java/com/android/icow/MainActivity.java
@@ -1,13 +1,60 @@
 package com.android.icow;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //toolbar
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+                //toolbar aktivieren
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("My title");
+
     }
+
+
+
+
+//button
+    public void button(View view) {
+        Intent intent = new Intent(this, Notiz.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    return true;}
+
+
+
+
+
+
+
+    //Inhalt
+
+
 }
---
+++ app/src/main/java/com/android/icow/Settings.java
@@ -0,0 +1,26 @@
+package com.android.icow;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Settings extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+}
