a963157599c395f07c7a72261ea17d86044d7be8
Soeren <soeren.lohmen@online.de>
2018-01-27 15:48:59 +0100

    Hintergrund ändern Option v1

--- app/src/main/java/com/android/icow/Settings.java
+++ app/src/main/java/com/android/icow/Settings.java
@@ -1,14 +1,26 @@
 package com.android.icow;
 
-import android.support.v7.app.ActionBar;
+
+
+
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class Settings extends AppCompatActivity {
 
+    View view ;
     Toolbar toolbar;
     ActionBar actionBar;
+    Button colorbutton;
+    TextView ändern;
+    int colornum = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +34,43 @@ public class Settings extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+
+
+
+        final Button colorbutton = (Button)findViewById(R.id.colorbutton);
+        colorbutton.setText("grau");
+        colorbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                color();
+                colornum++;
+
+            }
+        });}
+
+        private void color ()   {
+        final Button colorbutton = (Button)findViewById(R.id.colorbutton);
+        switch (colornum) {
+            case 1:
+                colorbutton.setText("blau");
+                view = this.getWindow().getDecorView();
+                view.setBackgroundResource(R.color.cardview_dark_background);
+                break;
+            case 2:
+                colorbutton.setText("weiß");
+                view = this.getWindow().getDecorView();
+                view.setBackgroundResource(R.color.colorPrimary);
+                break;
+            case 3:
+                colorbutton.setText("grau");
+                view = this.getWindow().getDecorView();
+                view.setBackgroundResource(R.color.cardview_light_background);
+                colornum=0;
+                break;
+        }
+
+        }
     }
-}
+
+
+
