b4c157660a0edfa93a38cfd29b5e128a8a8634cd
Soeren <soeren.lohmen@online.de>
2018-01-29 18:16:16 +0100

    Jetzt mit Switch.Preferences funzen noch nicht

--- app/src/main/java/com/android/icow/Settings.java
+++ app/src/main/java/com/android/icow/Settings.java
@@ -3,8 +3,7 @@ package com.android.icow;
 
 
 
-import android.app.Activity;
-import android.content.Context;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -14,9 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class Settings extends AppCompatActivity {
@@ -25,6 +24,7 @@ public class Settings extends AppCompatActivity {
     Toolbar toolbar;
     ActionBar actionBar;
     TextView ändern;
+    Switch switch1;
     private int colornum;
 
     public static final String PREFS_NAME = "MyPrefsFile";
@@ -35,13 +35,14 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        SharedPreferences sp = getSharedPreferences(PREFS_NAME, 0);
+       /* SharedPreferences sp = getSharedPreferences(PREFS_NAME, 0);
         colornum = sp.getInt("colornum", colornum);
         ändern = findViewById(R.id.ändern);
         ändern.setText(String.valueOf(colornum));
         color();
+        */
 
-
+        ändern = findViewById(R.id.ändern);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
@@ -50,76 +51,33 @@ public class Settings extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Einstellungen");
 
-        Button buttonDecrement = findViewById(R.id.decrement);
-        Button buttonIncrement = findViewById(R.id.increment);
-        ändern = findViewById(R.id.ändern);
 
+        switch1 = findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                colornum=1;
+                ändern.setText(String.valueOf(colornum));
+            } else {colornum =0;
+                    ändern.setText(String.valueOf(colornum));
+                    }
 
-        buttonIncrement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                increment();
                 color();
-            }
-
-        });
-
-        buttonDecrement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                decrement();
-                color();
-            }
-
-        });
-
-        if (savedInstanceState != null) {
-            colornum = savedInstanceState.getInt("bg");
-            ändern.setText(String.valueOf(colornum));
-            color();
         }
 
-    }
 
-    private void increment() {
-        if (colornum == 2) {
-            colornum++;
-            ändern.setText(String.valueOf(0));
-        } else {
-            colornum++;
-            ändern.setText(String.valueOf(colornum));
-        }
-
-    }
-
-    private void decrement() {
-        if (colornum == 0) {
-            colornum = 2;
-            ändern.setText(String.valueOf(colornum));
-        } else {
-            colornum--;
-            ändern.setText(String.valueOf(colornum));
-
-        }
+    });
     }
 
     private void color() {
-        //  final Button colorbutton = (Button)findViewById(R.id.colorbutton);
         ändern = findViewById(R.id.ändern);
-        colornum = Integer.parseInt(ändern.getText().toString());
+        colornum = Integer.valueOf(ändern.getText().toString());
         switch (colornum) {
             case 1:
-                // colorbutton.setText("blau");
                 view = this.getWindow().getDecorView();
                 view.setBackgroundResource(R.color.cardview_dark_background);
                 break;
-            case 2:
-                // colorbutton.setText("weiß");
-                view = this.getWindow().getDecorView();
-                view.setBackgroundResource(R.color.colorPrimary);
-                break;
             default:
-                //   colorbutton.setText("grau");
                 view = this.getWindow().getDecorView();
                 view.setBackgroundResource(R.color.cardview_light_background);
                 break;
@@ -143,24 +101,17 @@ public class Settings extends AppCompatActivity {
 
     }
 
-    public void layout() {
-        ändern = (TextView) findViewById(R.id.ändern);
-        colornum = Integer.parseInt(ändern.getText().toString());
-        Intent myintent = new Intent(Settings.this, MainActivity.class);
-        myintent.putExtra(EXTRA_COLORNUM, colornum);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-                    ändern = (TextView) findViewById(R.id.ändern);
-                    colornum = Integer.parseInt(ändern.getText().toString());
 
+                    ändern = findViewById(R.id.ändern);
+                    colornum = Integer.valueOf(ändern.getText().toString());
                     Intent myintent = new Intent(this, MainActivity.class);
                     myintent.putExtra(EXTRA_COLORNUM, colornum);
                     startActivity(myintent);
